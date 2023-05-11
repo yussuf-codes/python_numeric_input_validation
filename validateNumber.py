b2842b88810130179cdb93993a9deb241ac08600
yussuf-codes <yussuf.code@gmail.com>
2023-05-11 15:37:00 +0300

    initial commit

---
+++ validateNumber.py
@@ -0,0 +1,22 @@
+def validateType(object: object, objectType: type) -> bool:
+    try:
+        objectType(object)
+        return True
+    except ValueError:
+        return False
+
+
+def validateRange(number: int, numberRange: tuple[int]) -> bool:
+    if number >= numberRange[0] and number <= numberRange[1]:
+        return True
+    else:
+        return False
+
+
+def validateNumber(inputPrompt: str, numberType: type, numberRange=(float('-inf'), float('inf'))):
+    userInput = input(inputPrompt)
+    while True:
+        if validateType(userInput, numberType):
+            if validateRange(numberType(userInput), numberRange):
+                return numberType(userInput)
+        userInput = input(inputPrompt)
